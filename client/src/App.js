43fcfb41dc5167229ac2fe7dbedc5f115f643d15
Kc Sim <simkdriod@gmail.com>
2019-08-17 09:18:03 -0700

    Import apollo and wrap application around provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+// Components
 import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "localhost:4000/graphql"
+});
 
 export default class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Ninja's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
